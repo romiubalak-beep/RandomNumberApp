2df6f336b6ef7f5d7c6ea5e4b501d23dc790321c
romiubalak-beep <romiubalak@gmail.com>
2026-07-02 19:55:58 +0200

    Update MainActivity.cs
    إصلاح نهائي للكود

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,37 +1,36 @@
 using Android.App;
 using Android.Widget;
 using Android.OS;
-using Android.Views;
 using System.Security.Cryptography;
 
 [Activity(Label = "RandomApp", MainLauncher = true)]
 public class MainActivity : Activity
 {
-    protected override void OnCreate(Bundle? savedInstanceState)
+    protected override void OnCreate(Bundle savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
         
         // إنشاء TextView
-        var textView = new TextView(this);
+        TextView textView = new TextView(this);
         textView.Text = "اضغط على الزر";
         textView.TextSize = 30;
-        textView.SetGravity(GravityFlags.Center);
+        textView.Gravity = Android.Views.GravityFlags.Center;
         
         // إنشاء Button
-        var button = new Button(this);
+        Button button = new Button(this);
         button.Text = "توليد رقم عشوائي";
         
         // حدث الضغط
-        button.Click += (s, e) =>
+        button.Click += (sender, e) =>
         {
             int num = RandomNumberGenerator.GetInt32(1, 101);
             textView.Text = "الرقم: " + num;
         };
         
         // إنشاء Layout
-        var layout = new LinearLayout(this);
+        LinearLayout layout = new LinearLayout(this);
         layout.Orientation = Orientation.Vertical;
-        layout.SetGravity(GravityFlags.Center);
+        layout.Gravity = Android.Views.GravityFlags.Center;
         layout.SetPadding(50, 50, 50, 50);
         layout.AddView(textView);
         layout.AddView(button);
