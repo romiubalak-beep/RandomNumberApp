a6e1a32ccdd9e382c68ef35e637fc694beb747ec
romiubalak-beep <romiubalak@gmail.com>
2026-07-02 19:59:19 +0200

    Update MainActivity.cs
    إصلاح مشكلة Gravity

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,6 +1,7 @@
 using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Views;
 using System.Security.Cryptography;
 
 [Activity(Label = "RandomApp", MainLauncher = true)]
@@ -14,7 +15,7 @@ public class MainActivity : Activity
         TextView textView = new TextView(this);
         textView.Text = "اضغط على الزر";
         textView.TextSize = 30;
-        textView.Gravity = Android.Views.GravityFlags.Center;
+        textView.Gravity = GravityFlags.CenterHorizontal;
         
         // إنشاء Button
         Button button = new Button(this);
@@ -30,7 +31,7 @@ public class MainActivity : Activity
         // إنشاء Layout
         LinearLayout layout = new LinearLayout(this);
         layout.Orientation = Orientation.Vertical;
-        layout.Gravity = Android.Views.GravityFlags.Center;
+        layout.SetGravity(GravityFlags.Center);
         layout.SetPadding(50, 50, 50, 50);
         layout.AddView(textView);
         layout.AddView(button);
