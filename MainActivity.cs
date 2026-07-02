31651b7c97fb25924525fbf233e49991894a3a6f
romiubalak-beep <romiubalak@gmail.com>
2026-07-02 19:40:20 +0200

    Update MainActivity.cs
    تحديث الكود لاستخدام ملف التصميم

--- MainActivity.cs
+++ MainActivity.cs
@@ -6,26 +6,21 @@ using System.Security.Cryptography;
 [Activity(Label = "RandomApp", MainLauncher = true)]
 public class MainActivity : Activity
 {
+    private TextView textView;
+    private Button button;
+
     protected override void OnCreate(Bundle savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
-        
-        var tv = new TextView(this);
-        tv.Text = "اضغط على الزر";
-        tv.TextSize = 30;
-        
-        var btn = new Button(this);
-        btn.Text = "توليد رقم عشوائي";
-        
-        btn.Click += (s, e) => {
+        SetContentView(Resource.Layout.activity_main);
+
+        textView = FindViewById<TextView>(Resource.Id.textView);
+        button = FindViewById<Button>(Resource.Id.button);
+
+        button.Click += (s, e) =>
+        {
             int num = RandomNumberGenerator.GetInt32(1, 101);
-            tv.Text = "الرقم: " + num;
+            textView.Text = "الرقم: " + num;
         };
-        
-        var layout = new LinearLayout(this);
-        layout.AddView(tv);
-        layout.AddView(btn);
-        
-        SetContentView(layout);
     }
 }
