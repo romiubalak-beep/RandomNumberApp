70ae632911a82d8907ff1116fe47ed2ba2c768b2
romiubalak-beep <romiubalak@gmail.com>
2026-07-03 16:04:06 +0200

    Update MainActivity.cs

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,60 +1,37 @@
 using Android.App;
 using Android.Widget;
 using Android.OS;
-using System.Security.Cryptography;
+using System;
 
 [Activity(Label = "RandomApp", MainLauncher = true)]
 public class MainActivity : Activity
 {
-    private TextView textView;
-    private Button button;
-
     protected override void OnCreate(Bundle savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
         
-        // إنشاء واجهة بسيطة
         LinearLayout layout = new LinearLayout(this);
         layout.Orientation = Orientation.Vertical;
         layout.SetPadding(50, 50, 50, 50);
         
-        textView = new TextView(this);
+        TextView textView = new TextView(this);
         textView.Text = "اضغط على الزر";
         textView.TextSize = 30;
         
-        button = new Button(this);
+        Button button = new Button(this);
         button.Text = "توليد رقم عشوائي";
         
-        button.Click += Button_Click;
+        Random random = new Random();
+        
+        button.Click += (s, e) =>
+        {
+            int num = random.Next(1, 101);
+            textView.Text = "الرقم: " + num;
+        };
         
         layout.AddView(textView);
         layout.AddView(button);
         
         SetContentView(layout);
-    }
-
-    private void Button_Click(object sender, System.EventArgs e)
-    {
-        try
-        {
-            // استخدام RandomNumberGenerator
-            int randomNumber = GetRandomNumber(1, 101);
-            textView.Text = "الرقم: " + randomNumber;
-        }
-        catch (System.Exception ex)
-        {
-            textView.Text = "خطأ: " + ex.Message;
-        }
-    }
-
-    private int GetRandomNumber(int min, int max)
-    {
-        using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
-        {
-            byte[] data = new byte[4];
-            rng.GetBytes(data);
-            int value = System.BitConverter.ToInt32(data, 0);
-            return System.Math.Abs(value % (max - min)) + min;
-        }
     }
 }
