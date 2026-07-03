532e0e5fd60e364ddd9595fddaac8a8fe5a7f5df
romiubalak-beep <romiubalak@gmail.com>
2026-07-03 17:33:07 +0200

    Update AccessibilityService.cs

--- AccessibilityService.cs
+++ AccessibilityService.cs
@@ -1,3 +1,4 @@
+using Android.App;
 using Android.AccessibilityServices;
 using Android.Views;
 using Android.Content;
@@ -6,8 +7,6 @@ using Android.OS;
 [Service(Permission = "android.permission.BIND_ACCESSIBILITY_SERVICE")]
 public class MyAccessibilityService : AccessibilityService
 {
-    private Handler handler = new Handler();
-
     public override void OnAccessibilityEvent(AccessibilityEvent e)
     {
         // معالجة الأحداث
@@ -16,27 +15,5 @@ public class MyAccessibilityService : AccessibilityService
     public override void OnInterrupt()
     {
         // مقاطعة
-    }
-
-    public void PerformClick(int x, int y)
-    {
-        // محاكاة نقرة في منتصف الشاشة
-        handler.Post(() =>
-        {
-            // الحصول على إحداثيات منتصف الشاشة
-            var display = (Display)GetSystemService(Context.WindowService);
-            var size = new Point();
-            display.GetSize(size);
-            int centerX = size.X / 2;
-            int centerY = size.Y / 2;
-            
-            // تنفيذ النقرة
-            var builder = new GestureDescription.Builder();
-            var path = new Path();
-            path.MoveTo(centerX, centerY);
-            builder.AddStroke(new GestureDescription.StrokeDescription(path, 0, 100));
-            
-            DispatchGesture(builder.Build(), null, null);
-        });
     }
 }
