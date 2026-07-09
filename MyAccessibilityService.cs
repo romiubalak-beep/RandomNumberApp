7622524d0a752b3292994591ddf3587ef06196cf
romiubalak-beep <romiubalak@gmail.com>
2026-07-09 14:57:39 +0200

    Update MyAccessibilityService.cs

--- MyAccessibilityService.cs
+++ MyAccessibilityService.cs
@@ -1,11 +1,17 @@
 using Android.AccessibilityServices;
+using Android.Runtime;
 using Android.Views.Accessibility;
 
 namespace RandomNumberApp;
 
+[Register("com.example.randomapp.MyAccessibilityService")]
 [Service(
     Permission = "android.permission.BIND_ACCESSIBILITY_SERVICE",
     Exported = true)]
+[IntentFilter(new[]
+{
+    "android.accessibilityservice.AccessibilityService"
+})]
 public class MyAccessibilityService : AccessibilityService
 {
     public static MyAccessibilityService? Instance { get; private set; }
@@ -13,12 +19,7 @@ public class MyAccessibilityService : AccessibilityService
     protected override void OnServiceConnected()
     {
         base.OnServiceConnected();
-
         Instance = this;
-
-        Android.Util.Log.Debug(
-            "ACCESSIBILITY",
-            "Service Connected");
     }
 
     public override void OnAccessibilityEvent(AccessibilityEvent e)
