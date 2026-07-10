45399967164c71009429b5967c87a8bdd66a74f2
romiubalak-beep <romiubalak@gmail.com>
2026-07-10 07:17:54 +0200

    Update MyAccessibilityService.cs

--- MyAccessibilityService.cs
+++ MyAccessibilityService.cs
@@ -1,11 +1,20 @@
 using Android.AccessibilityServices;
+using Android.App;
 using Android.Views.Accessibility;
 
 namespace RandomNumberApp;
 
 [Service(
     Permission = "android.permission.BIND_ACCESSIBILITY_SERVICE",
-    Exported = true)]
+    Exported = true,
+    Label = "RandomApp Accessibility")]
+[IntentFilter(
+    new[] {
+        "android.accessibilityservice.AccessibilityService"
+    })]
+[MetaData(
+    "android.accessibilityservice",
+    Resource = "@xml/accessibility_service_config")]
 public class MyAccessibilityService : AccessibilityService
 {
     public static MyAccessibilityService? Instance { get; private set; }
